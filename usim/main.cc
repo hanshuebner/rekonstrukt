e3e1473e25383bcdddcc9a94616180670221cc44
hans.huebner <hans.huebner@294b93ee-f50c-11dd-a8e2-f7cb3c35fcff>
2009-02-08 12:07:14 +0000

    Make compilable without X11. Make UART base address configurable.
    git-svn-id: https://rekonstrukt.googlecode.com/svn/trunk@9 294b93ee-f50c-11dd-a8e2-f7cb3c35fcff

--- usim/main.cc
+++ usim/main.cc
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#ifdef X11
 #include "mc6809_X.h"
+#else
+#include "mc6809.h"
+#endif
 #include "mc6850.h"
 
 #ifdef __unix
@@ -16,74 +20,83 @@
 extern "C" unsigned int alarm(unsigned int);
 #endif
 
+#define UART_ADDR 0xB000
+
 //#ifndef sun
 //typedef void SIG_FUNC_TYP(int);
 //typedef SIG_FUNC_TYPE *SIG_FP;
 //#endif
 
-class sys : virtual public mc6809_X {
+class sys
+  : virtual public
+#ifdef X11
+mc6809_X
+#else
+mc6809
+#endif
+{
 
 protected:
 
-	virtual Byte			 read(Word);
-	virtual void			 write(Word, Byte);
+  virtual Byte			 read(Word);
+  virtual void			 write(Word, Byte);
 
 protected:
 
-		mc6850			 uart;
+  mc6850			 uart;
 
 } sys;
 
 Byte sys::read(Word addr)
 {
-	Byte		ret = 0;
+  Byte		ret = 0;
 
-	if ((addr & 0xfffe) == 0xc000) {
-		ret = uart.read(addr);
-	} else {
-		ret = mc6809_X::read(addr);
-	}
+  if ((addr & 0xfffe) == UART_ADDR) {
+    ret = uart.read(addr);
+  } else {
+    ret = mc6809::read(addr);
+  }
 
-	return ret;
+  return ret;
 }
 
 void sys::write(Word addr, Byte x)
 {
-	if ((addr & 0xfffe) == 0xc000) {
-		uart.write(addr, x);
-	} else {
-		mc6809_X::write(addr, x);
-	}
+  if ((addr & 0xfffe) == UART_ADDR) {
+    uart.write(addr, x);
+  } else {
+    mc6809::write(addr, x);
+  }
 }
 
 #ifdef SIGALRM
 #ifdef sun
 void update(int, ...)
 #else
-void update(int)
+  void update(int)
 #endif
 {
-	sys.status();
-	(void)signal(SIGALRM, update);
-	alarm(1);
+  sys.status();
+  (void)signal(SIGALRM, update);
+  alarm(1);
 }
 #endif // SIGALRM
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		fprintf(stderr, "usage: usim <hexfile>\n");
-		return EXIT_FAILURE;
-	}
+  if (argc != 2) {
+    fprintf(stderr, "usage: usim <hexfile>\n");
+    return EXIT_FAILURE;
+  }
 
-	(void)signal(SIGINT, SIG_IGN);
+  (void)signal(SIGINT, SIG_IGN);
 #ifdef SIGALRM
-	(void)signal(SIGALRM, update);
-	alarm(1);
+  (void)signal(SIGALRM, update);
+  alarm(1);
 #endif
 
-	sys.load_intelhex(argv[1]);
-	sys.run();
+  sys.load_intelhex(argv[1]);
+  sys.run();
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
