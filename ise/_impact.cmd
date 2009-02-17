25498d6bb796ff1a7e46911cebeae842c1038d7a
hans.huebner <hans.huebner@294b93ee-f50c-11dd-a8e2-f7cb3c35fcff>
2009-02-17 23:08:37 +0000

    checkpoint ise files
    git-svn-id: https://rekonstrukt.googlecode.com/svn/trunk@35 294b93ee-f50c-11dd-a8e2-f7cb3c35fcff

--- ise/_impact.cmd
+++ ise/_impact.cmd
@@ -32,10 +32,27 @@ setAttribute -configdevice -attr path -value "C:\hans\rekonstrukt\ise\/"
 setAttribute -configdevice -attr name -value "my_system09.mcs"
 generate
 setCurrentDesign -version 0
+setMode -pff
+setSubmode -pffserial
+setAttribute -configdevice -attr fillValue -value "FF"
+setAttribute -configdevice -attr fileFormat -value "mcs"
+setAttribute -configdevice -attr dir -value "UP"
+setAttribute -configdevice -attr path -value "C:\hans\rekonstrukt\ise\/"
+setAttribute -configdevice -attr name -value "my_system09.mcs"
+generate
+setCurrentDesign -version 0
+setMode -pff
+setSubmode -pffserial
+setAttribute -configdevice -attr fillValue -value "FF"
+setAttribute -configdevice -attr fileFormat -value "mcs"
+setAttribute -configdevice -attr dir -value "UP"
+setAttribute -configdevice -attr path -value "C:\hans\rekonstrukt\ise\/"
+setAttribute -configdevice -attr name -value "my_system09.mcs"
+generate
+setCurrentDesign -version 0
 setMode -bs
 setMode -bs
 setCable -port auto
 Program -p 2 -e -v 
-Program -p 1 
 setMode -pff
 setMode -pff
--- ise/my_system09.sig
+++ ise/my_system09.sig
@@ -1,7 +1,7 @@
 // PROMGEN: Xilinx Prom Generator K.39
 // Copyright (c) 1995-2008 Xilinx, Inc.  All rights reserved.
 
-DATE      02/16/09-07:10
+DATE      02/17/09-10:17
 SOURCE    C:\hans\rekonstrukt\ise\//my_system09.mcs
 DEVICE    XCF04S
-SIGNATURE 0x03FAC12C
+SIGNATURE 0x03FB5BBE
